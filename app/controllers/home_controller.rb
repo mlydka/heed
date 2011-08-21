7784f082e45ba3b9f5d2deffeca5b2be480b7b56
mlydka <michal.lydka@gmail.com>
2011-08-21 20:39:52 +0100

    add jquery 1.6.2

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
 
   def index
-    render :text => 'Welcome'
   end
 
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,8 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+var Application = {
+    init: function() {
+    }
+};
+
+$(document).ready(function(){
+  Application.init();
+});
