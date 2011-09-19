984c7533c3c0cdda5d3af049f7f51716054793ae
mlydka <michal.lydka@gmail.com>
2011-09-19 23:40:53 +0100

    fix JS issues

--- public/javascripts/simpleSlider.js
+++ public/javascripts/simpleSlider.js
@@ -7,8 +7,6 @@
 
     return this.each(function() {
       var slider = $(this);
-      var active = getActive();
-      var next   = getNext();
 
       function getActive() {
         var active = slider.find('img.active');
@@ -16,11 +14,15 @@
       };
 
       function getNext() {
+	var active = getActive();
         var next = active.next();
-	return next.length == 0 ? slider.find('img:fist') : next;
+	return next.length == 0 ? slider.find('img:first') : next;
       };
 
       function autoSlide() {
+	var active = getActive();
+	var next   = getNext();
+
         active.addClass('last-active');
 	next.css({opacity: 0.0}).addClass('active').animate({opacity: 1.0}, 1000, function() {
 	  active.removeClass('active last-active');
